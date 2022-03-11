33d38b24475f1d860b9a681e8e2a230889032e02
Evaline Wangu <evalinemacharia99@gmail.com>
2022-03-11 16:53:30 +0300

    Add styles to background image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-$(document).ready(function(){
-    $(".design").click(function(){
-        $(".design").hide();
-        $(".p").toggle();
-        $(".p1").show();
-    }
-}
-
-))
+$(document).ready
