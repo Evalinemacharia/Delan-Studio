daa2229600179f8ee7686cf70eff7aeee503bb62
Evaline Wangu <evalinemacharia99@gmail.com>
2022-03-11 18:18:02 +0300

    Add column div class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-$(document).ready
+$(document).ready(function(){
+    $(".design").click(function(){
+      $(".design").hide();
+      $(".p1").toggle();
+      $(".p1").show();
+    });
+
+    $(".p1").click(function(){
+        $(".p1").hide(1200);
+        $(".design").toggle(1200);
+        $(".design").show(1200);
+      });    
+}
+
+)
